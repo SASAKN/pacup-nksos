dd5e4ab86bd6b8dcea1167166afa36e86e0f0f72
SASAKEN <114722212+SASAKN@users.noreply.github.com>
2023-08-11 18:38:25 +0900

    インストールに必要なチェック処理にかからないために、削除。

--- install_pacup.sh
+++ install_pacup.sh
@@ -1,19 +1,5 @@
 #!/bin/bash
 
-if [ ! -f /etc/debian_version ];then
-  echo -e "このディストリビューションには､ \"pacup\" をインストール出来ません｡\n\nDebian系 もしくは Debian派生系のみ対応しています｡"
-  exit 1
-fi
-
-if [ `whoami` != 'root' ]; then
-  sudo $BASH_SOURCE
-  if [ $? != 0 ]; then
-    echo "Root(管理者)権限で実行してください"
-    exit 1
-  fi
-  exit 0
-fi
-
 if [ ! -f ./pacup ]; then
   echo -e "\"pacup\" ファイルが見つかりませんでした\n"
   exit 1
@@ -36,15 +22,6 @@ if [ ! -d /usr/local/share/applications ]; then
 fi
 
 cp -f ./pacup.desktop /usr/local/share/applications
-
-if [ -d ../デスクトップ ]; then
-    cp -f ./pacup_Manual.txt ../デスクトップ
-elif [ -d ../Desktop ]; then
-    cp -f ./pacup_Manual.txt ../Desktop
-else
-    echo "マニュアルファイルのコピーをスキップしました"
-    exit 1
-fi
 
 echo "Success!"
 exit 0
